a0cf16e03f341cde6d54541e07507b57fe992097
jumy <yygutn@gmail.com>
2016-08-16 13:00:46 +0800

    Bug修复: 开启App，启用账号密码登录，会自动登录上一次退出App时账号的Bug

--- app/src/main/java/com/hyphenate/chatui/ui/LoginActivity.java
+++ app/src/main/java/com/hyphenate/chatui/ui/LoginActivity.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,23 +40,22 @@ import cn.edu.jumy.oa.OAService;
 
 /**
  * 登陆页面
- * 
  */
 public class LoginActivity extends BaseActivity {
-	private static final String TAG = "LoginActivity";
-	public static final int REQUEST_CODE_SETNICK = 1;
-	private EditText usernameEditText;
-	private EditText passwordEditText;
+    private static final String TAG = "LoginActivity";
+    public static final int REQUEST_CODE_SETNICK = 1;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
 
-	private boolean progressShow;
-	private boolean autoLogin = false;
+    private boolean progressShow;
+    private boolean autoLogin = false;
 
-	private String currentUsername;
-	private String currentPassword;
+    private String currentUsername;
+    private String currentPassword;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
 //		// 如果登录成功过，直接进入主页面
 //		if (DemoHelper.getInstance().isLoggedIn()) {
@@ -64,15 +63,15 @@ public class LoginActivity extends BaseActivity {
 //			startActivity(new Intent(LoginActivity.this, cn.edu.jumy.oa.MainActivity.class));
 //			return;
 //		}
-		try {
-			AppManager.getInstance().finishAllBesideTop();
-			setContentView(R.layout.em_activity_login);
+        try {
+            AppManager.getInstance().finishAllBesideTop();
+            setContentView(R.layout.em_activity_login);
 
-			usernameEditText = (EditText) findViewById(R.id.username);
-			passwordEditText = (EditText) findViewById(R.id.password);
+            usernameEditText = (EditText) findViewById(R.id.username);
+            passwordEditText = (EditText) findViewById(R.id.password);
 
-			// 如果用户名改变，清空密码
-			usernameEditText.addTextChangedListener(new TextWatcher() {
+            // 如果用户名改变，清空密码
+            usernameEditText.addTextChangedListener(new TextWatcher() {
                 @Override
                 public void onTextChanged(CharSequence s, int start, int before, int count) {
                     passwordEditText.setText(null);
@@ -88,130 +87,158 @@ public class LoginActivity extends BaseActivity {
 
                 }
             });
-			if (DemoHelper.getInstance().getCurrentUsernName() != null) {
+            if (DemoHelper.getInstance().getCurrentUsernName() != null) {
                 usernameEditText.setText(DemoHelper.getInstance().getCurrentUsernName());
             }
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * 登录
-	 * 
-	 * @param view
-	 */
-	public void login(View view) {
-		if (!EaseCommonUtils.isNetWorkConnected(this)) {
-			Toast.makeText(this, R.string.network_isnot_available, Toast.LENGTH_SHORT).show();
-			return;
-		}
-		currentUsername = usernameEditText.getText().toString().trim();
-		currentPassword = passwordEditText.getText().toString().trim();
+    /**
+     * 登录
+     */
+    public void login(final View view) {
 
-		if (TextUtils.isEmpty(currentUsername)) {
-			Toast.makeText(this, R.string.User_name_cannot_be_empty, Toast.LENGTH_SHORT).show();
-			return;
-		}
-		if (TextUtils.isEmpty(currentPassword)) {
-			Toast.makeText(this, R.string.Password_cannot_be_empty, Toast.LENGTH_SHORT).show();
-			return;
-		}
+        final ProgressDialog pd = new ProgressDialog(LoginActivity.this);
+        pd.setCanceledOnTouchOutside(false);
+        pd.setOnCancelListener(new OnCancelListener() {
 
-		progressShow = true;
-		final ProgressDialog pd = new ProgressDialog(LoginActivity.this);
-		pd.setCanceledOnTouchOutside(false);
-		pd.setOnCancelListener(new OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                Log.d(TAG, "EMClient.getInstance().onCancel");
+                progressShow = false;
+            }
+        });
+        pd.setMessage(getString(R.string.Is_landing));
+        pd.show();
 
-			@Override
-			public void onCancel(DialogInterface dialog) {
-				Log.d(TAG, "EMClient.getInstance().onCancel");
-				progressShow = false;
-			}
-		});
-		pd.setMessage(getString(R.string.Is_landing));
-		pd.show();
+        if (DemoHelper.getInstance().isLoggedIn()) {
+            DemoHelper.getInstance().logout(false, new EMCallBack() {
+                @Override
+                public void onSuccess() {
+                    LoginActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            doLogin(pd);
+                        }
+                    });
+                }
 
-		// After logout，the DemoDB may still be accessed due to async callback, so the DemoDB will be re-opened again.
-		// close it before login to make sure DemoDB not overlap
+                @Override
+                public void onError(int i, String s) {
+                    LoginActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            login(view);
+                        }
+                    });
+                }
+
+                @Override
+                public void onProgress(int i, String s) {
+
+                }
+            });
+        } else {
+            doLogin(pd);
+        }
+    }
+
+    private void doLogin(final ProgressDialog pd) {
+        if (!EaseCommonUtils.isNetWorkConnected(this)) {
+            Toast.makeText(this, R.string.network_isnot_available, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        currentUsername = usernameEditText.getText().toString().trim();
+        currentPassword = passwordEditText.getText().toString().trim();
+
+        if (TextUtils.isEmpty(currentUsername)) {
+            Toast.makeText(this, R.string.User_name_cannot_be_empty, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(currentPassword)) {
+            Toast.makeText(this, R.string.Password_cannot_be_empty, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressShow = true;
+
+        // After logout，the DemoDB may still be accessed due to async callback, so the DemoDB will be re-opened again.
+        // close it before login to make sure DemoDB not overlap
         DemoDBManager.getInstance().closeDB();
 
         // reset current user name before login
         DemoHelper.getInstance().setCurrentUserName(currentUsername);
-        
-		final long start = System.currentTimeMillis();
-		// 调用sdk登陆方法登陆聊天服务器
-		Log.d(TAG, "EMClient.getInstance().login");
-		EMClient.getInstance().login(currentUsername, currentPassword, new EMCallBack() {
 
-			@Override
-			public void onSuccess() {
-				Log.d(TAG, "login: onSuccess");
-				DemoApplication.currentUserName = currentUsername;
-				OAService.username = EMClient.getInstance().getCurrentUser() + "_";
-				if (!LoginActivity.this.isFinishing() && pd.isShowing()) {
-					pd.dismiss();
-				}
+//		final long start = System.currentTimeMillis();
+        // 调用sdk登陆方法登陆聊天服务器
+        Log.d(TAG, "EMClient.getInstance().login");
+        EMClient.getInstance().login(currentUsername, currentPassword, new EMCallBack() {
 
-				// ** 第一次登录或者之前logout后再登录，加载所有本地群和回话
-				// ** manually load all local groups and
-			    EMClient.getInstance().groupManager().loadAllGroups();
-			    EMClient.getInstance().chatManager().loadAllConversations();
-				
-				// 更新当前用户的nickname 此方法的作用是在ios离线推送时能够显示用户nick
-				boolean updatenick = EMClient.getInstance().updateCurrentUserNick(
-						DemoApplication.currentUserNick.trim());
-				if (!updatenick) {
-					Log.e("LoginActivity", "update current user nick fail");
-				}
-				//异步获取当前用户的昵称和头像(从自己服务器获取，demo使用的一个第三方服务)
-				DemoHelper.getInstance().getUserProfileManager().asyncGetCurrentUserInfo();
+            @Override
+            public void onSuccess() {
+                Log.d(TAG, "login: onSuccess");
+                DemoApplication.currentUserName = currentUsername;
+                OAService.username = EMClient.getInstance().getCurrentUser() + "_";
+                if (!LoginActivity.this.isFinishing() && pd.isShowing()) {
+                    pd.dismiss();
+                }
 
-				// 进入主页面
-				Intent intent = new Intent(LoginActivity.this,
-						cn.edu.jumy.oa.MainActivity.class);
-				startActivity(intent);
+                // ** 第一次登录或者之前logout后再登录，加载所有本地群和回话
+                // ** manually load all local groups and
+                EMClient.getInstance().groupManager().loadAllGroups();
+                EMClient.getInstance().chatManager().loadAllConversations();
 
-				AppManager.getInstance().finishCurActivity();
-			}
+                // 更新当前用户的nickname 此方法的作用是在ios离线推送时能够显示用户nick
+                boolean updatenick = EMClient.getInstance().updateCurrentUserNick(
+                        DemoApplication.currentUserNick.trim());
+                if (!updatenick) {
+                    Log.e("LoginActivity", "update current user nick fail");
+                }
+                //异步获取当前用户的昵称和头像(从自己服务器获取，demo使用的一个第三方服务)
+                DemoHelper.getInstance().getUserProfileManager().asyncGetCurrentUserInfo();
 
-			@Override
-			public void onProgress(int progress, String status) {
-				Log.d(TAG, "login: onProgress");
-			}
+                // 进入主页面
+                Intent intent = new Intent(LoginActivity.this,
+                        cn.edu.jumy.oa.MainActivity.class);
+                startActivity(intent);
+            }
 
-			@Override
-			public void onError(final int code, final String message) {
-				Log.d(TAG, "login: onError: " + code);
-				if (!progressShow) {
-					return;
-				}
-				runOnUiThread(new Runnable() {
-					public void run() {
-						pd.dismiss();
-						Toast.makeText(getApplicationContext(), getString(R.string.Login_failed) + message,
-								Toast.LENGTH_SHORT).show();
-					}
-				});
-			}
-		});
-	}
+            @Override
+            public void onProgress(int progress, String status) {
+                Log.d(TAG, "login: onProgress");
+            }
 
-	
-	/**
-	 * 注册
-	 * 
-	 * @param view
-	 */
-	public void register(View view) {
-		startActivityForResult(new Intent(this, RegisterActivity.class), 0);
-	}
+            @Override
+            public void onError(final int code, final String message) {
+                Log.d(TAG, "login: onError: " + code);
+                if (!progressShow) {
+                    return;
+                }
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        pd.dismiss();
+                        Toast.makeText(getApplicationContext(), getString(R.string.Login_failed) + message,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		if (autoLogin) {
-			return;
-		}
-	}
+
+    /**
+     * 注册
+     *
+     * @param view
+     */
+    public void register(View view) {
+//		startActivityForResult(new Intent(this, RegisterActivity.class), 0);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
