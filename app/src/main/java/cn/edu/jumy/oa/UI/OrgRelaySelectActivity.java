d07cb38f2d28f53103f9341022ecf87ce5eca521
jumy <yygutn@gmail.com>
2016-08-17 10:13:03 +0800

    需求修改: 把转发的“备注”改为“转发意见”。

--- app/src/main/java/cn/edu/jumy/oa/UI/OrgRelaySelectActivity.java
+++ app/src/main/java/cn/edu/jumy/oa/UI/OrgRelaySelectActivity.java
@@ -149,7 +149,7 @@ public class OrgRelaySelectActivity extends BaseActivity {
         final String IDS = ids;
         final EditText editText = new EditText(mContext);
         AlertDialog dialog = new AlertDialog.Builder(mContext)
-                .setTitle("备注")
+                .setTitle("转发意见")
                 .setView(editText)
                 .setPositiveButton("确认", new DialogInterface.OnClickListener() {
                     @Override
